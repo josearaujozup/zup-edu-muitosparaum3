1fd41bac1cef84708d41180447a851597e3027c5
Denes <jose.araujo@zup.com.br>
2022-04-01 09:11:39 -0300

    criado o projeto de avaliacoes de alunos

---
+++ src/main/java/br/com/zup/edu/avaliacoesalunos/AvaliacoesalunosApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.avaliacoesalunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacoesalunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacoesalunosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/edu/avaliacoesalunos/AvaliacoesalunosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.avaliacoesalunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacoesalunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
